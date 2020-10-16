96080ed6c20058b60b77c5aadb16dc4535a8215a
jaimin.rathod <3331-jaimin.rathod@users.noreply.source.golabs.io>
2020-10-16 15:14:37 +0530

    Add main class that takes input

---
+++ src/main/java/com.gojek.bootcamp007.gameoflife/Main.java
@@ -0,0 +1,40 @@
+package com.gojek.bootcamp007.gameoflife;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input file path");
+        final String inputFilePath = sc.nextLine();
+
+        Set<Point> livePointsSet = new HashSet<>();
+
+        File file = new File(inputFilePath);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        int lineCounter = 0;
+        int hashtagCount = 0;
+        while ((line = br.readLine()) != null) {
+            for (int i = 0; i < line.length(); i++) {
+                if (line.charAt(i) == '#') {
+                    livePointsSet.add(new Point(lineCounter, i));
+                    hashtagCount++;
+                }
+            }
+            lineCounter++;
+        }
+        fr.close();
+
+        Point[] livePoints = new Point[hashtagCount];
+        int counter = 0;
+        for (Point livePoint : livePointsSet) {
+            livePoints[counter] = livePoint;
+            counter++;
+        }
+    }
+}
